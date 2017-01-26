fc843d2a0fdc14ef39b20cceb3eb48b1267a8672
Ben Sterrett <raynjamin@gmail.com>
2017-01-26 13:34:45 -0500

    Adding irongram project.

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,22 @@
+function getUser(userData) {
+    if (userData.length == 0) {
+        $("#login").show();
+
+    }
+    else {
+        $("#logout").show();
+        $("#upload").show();
+    }
+}
+
+function getPhotos(photosData) {
+    for (var i in photosData) {
+        var photo = photosData[i];
+        var elem = $("<img>");
+        elem.attr("src", photo.filename);
+        $("#photos").append(elem);
+    }
+}
+
+$.get("/user", getUser);
+$.get("/photos", getPhotos);
